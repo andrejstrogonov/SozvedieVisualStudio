971f362b577fa7b841d8a1590cb5d5dbd47337d6
andrejstrogonov <strogonovandrej@hotmail.com>
2026-03-16 17:10:16 +0300

    Добавьте файлы проекта.

---
+++ SozvedieVisualStudio/SozvedieVisualStudio.cpp
@@ -0,0 +1,12 @@
+﻿// SozvedieVisualStudio.cpp: определяет точку входа для приложения.
+//
+
+#include "SozvedieVisualStudio.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
